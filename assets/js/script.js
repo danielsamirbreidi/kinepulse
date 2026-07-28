8d3fab18ad008e377cd29a737c1cf3219d82b2a4
danielsamirbreidi <danielsamirbreidi@gmail.com>
2026-07-28 13:58:07 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+const observer = new IntersectionObserver((entries)=>{
 
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".service-card").forEach(el=>{
+
+el.classList.add("fade-up");
+
+observer.observe(el);
+
+});
+
+document.querySelectorAll(".why-grid div").forEach(el=>{
+
+el.classList.add("fade-up");
+
+observer.observe(el);
+
+});
